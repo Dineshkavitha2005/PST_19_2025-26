89e03d6a409a58054d2874326b46a73db72c9ff5
A.Dinesh <147620513+Dineshkavitha2005@users.noreply.github.com>
2026-01-12 21:48:29 +0530

    Add files via upload

---
+++ Conditional statements/multiplication table of num.java
@@ -0,0 +1,10 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int num;
+	    num=4;
+	    for (int i=1;i<=20;i++)
+		System.out.println(num + "x" + i + "=" + (num * i));
+	}
+}
