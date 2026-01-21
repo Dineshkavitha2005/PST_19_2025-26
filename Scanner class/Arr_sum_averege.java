ad9b5c57dbf14ace84461122a2a4b937e49dbeb8
A.Dinesh <147620513+Dineshkavitha2005@users.noreply.github.com>
2026-01-21 21:06:33 +0530

    Add files via upload

---
+++ Scanner class/Arr_sum_averege.java
@@ -0,0 +1,24 @@
+package PST;
+
+import java.util.Scanner;
+
+public class Arr_sum_averege {
+	public static void main(String []args) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter an size of an element:");
+	int a=sc.nextInt();
+	sc.nextLine();
+	int arr[]=new int[a];
+	System.out.println("Enter an number:");
+	int sum=0;
+	for(int i=0;i<a;i++) {
+		
+		arr[i]=sc.nextInt();
+		 sum +=arr[i]; 
+	}
+	double average =sum/a;
+	System.out.println("The sum of numbers is:"+sum);
+		 System.out.println("the average of two numbers is:"+average);
+	
+	sc.close();
+}}
