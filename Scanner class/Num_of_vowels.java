d42de983ba93910522091dd6cd3264c4021e2291
A.Dinesh <147620513+Dineshkavitha2005@users.noreply.github.com>
2026-01-22 09:40:06 +0530

    Add files via upload

---
+++ Scanner class/Num_of_vowels.java
@@ -0,0 +1,28 @@
+package PST;
+
+import java.util.Scanner;
+
+public class Num_of_vowels {
+	public static void main(String args[]) {
+		
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a String:");
+		String a=sc.nextLine();
+		int vowelcount=0;
+		
+		for(int i=0;i<a.length();i++) {
+			char ch=a.charAt(i);
+			if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+			{
+			vowelcount++;
+		}}
+		if(vowelcount>0) {
+			System.out.println("The given string contains vowels:"+vowelcount);
+		}
+		else 
+		{
+			System.out.println("The given string has only consonents.");
+		}
+		sc.close();
+}}
