4d3d44cd36b847a5a000f9a2fb2adac359c256d0
Dinesh A <dineshkavitha2005@gmail.com>
2026-02-05 22:36:47 +0530

    Add files via upload

---
+++ OOP Concept/New_method.java
@@ -0,0 +1,27 @@
+package OOP_concept;
+
+public class New_method {
+	
+	public static void main(String[] args) {
+		lohith();
+	}
+public static void lohith() {
+	System.out.println("Lohith is phone call boy. ");
+	jayaprakash();
+}
+public static void jayaprakash() {
+	System.out.println("Jayaprakash is gundu boy. ");
+	prajith();
+}
+public static void prajith() {
+	System.out.println("Prajith is sick boy.");
+	harish();
+}
+public static void harish() {
+	System.out.println("Harish is love failure boy.");
+	ajin();
+}
+public static void ajin() {
+	System.out.println("Ajin is lover boy.");
+}
+}
