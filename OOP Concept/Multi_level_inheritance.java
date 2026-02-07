5f7667de74d73af449739ac86af0efed0bf5eef8
Dinesh A <dineshkavitha2005@gmail.com>
2026-02-07 20:51:40 +0530

    Add files via upload

---
+++ OOP Concept/Multi_level_inheritance.java
@@ -0,0 +1,30 @@
+package OOP_concept;
+
+public class Multi_level_inheritance {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
+class ab{
+	void exe_1() {
+		
+	}
+}
+class bc extends ab{
+	void exe_2() {
+		
+	}
+}
+class cd extends bc{
+	void exe_3() {
+		
+	}
+}
+class de extends cd{
+	void exe_4() {
+		
+	}
+}
