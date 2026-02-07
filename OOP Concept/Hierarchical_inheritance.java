1a08470fe29af6a6d14735727ce829b3defb749d
Dinesh A <dineshkavitha2005@gmail.com>
2026-02-07 20:51:20 +0530

    Add files via upload

---
+++ OOP Concept/Hierarchical_inheritance.java
@@ -0,0 +1,25 @@
+package OOP_concept;
+
+public class Hierarchical_inheritance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+class cat_1{
+	void new_1() {
+		
+	}
+}
+class cat_2 extends cat_1{
+	void new_2() {
+		
+	}
+}
+class cat_3 extends cat_1{
+	void new_3() {
+		
+	}
+}
