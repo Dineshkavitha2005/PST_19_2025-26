3ab05465fd40bd0ca6159fff9ff2e852b3972f51
Dinesh A <dineshkavitha2005@gmail.com>
2026-02-13 21:36:02 +0530

    Add files via upload

---
+++ Patterns/pattern_diagonal.java
@@ -0,0 +1,22 @@
+package Pattern;
+import java.util.Scanner;
+public class pattern_diagonal {
+public static void main(String args[]) {
+	System.out.println(diagonal());
+}
+static String diagonal(){
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter the number:");
+	int num=sc.nextInt();
+	for(int i=1;i<num;i++) {
+		System.out.println(" * ");
+		for(int j=1;j<i+1;j++){
+			System.out.print(" * ");
+			
+		}	
+	}
+	sc.close();
+	return " * ";
+	
+}
+}
